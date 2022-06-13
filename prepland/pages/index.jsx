5e657fcb4956d72fdb3e2122e0eb36fdbf93f156
AtlanticNova <idiotcolony97@gmail.com>
2022-06-13 17:51:55 +0700

    Update Index.jsx

---
+++ prepland/pages/index.jsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import { Title, Text, Container, Button, Overlay, createStyles } from '@mantine/core';
+import HeaderTabs from '../components/AppHeader';
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    position: 'relative',
+    paddingTop: 303,
+    paddingBottom: 300,
+    backgroundImage:
+      'url(https://images.unsplash.com/photo-1591189863430-ab87e120f312?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+
+    '@media (max-width: 520px)': {
+      paddingTop: 80,
+      paddingBottom: 50,
+    },
+  },
+
+  inner: {
+    position: 'relative',
+    zIndex: 1,
+  },
+
+  title: {
+    fontWeight: 800,
+    fontSize: 40,
+    letterSpacing: -1,
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    color: theme.white,
+    marginBottom: theme.spacing.xs,
+    textAlign: 'center',
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    '@media (max-width: 520px)': {
+      fontSize: 28,
+      textAlign: 'left',
+    },
+  },
+
+  highlight: {
+    color: theme.colors[theme.primaryColor][4],
+  },
+
+  description: {
+    color: theme.colors.gray[0],
+    textAlign: 'center',
+
+    '@media (max-width: 520px)': {
+      fontSize: theme.fontSizes.md,
+      textAlign: 'left',
+    },
+  },
+
+  controls: {
+    marginTop: theme.spacing.xl * 1.5,
+    display: 'flex',
+    justifyContent: 'center',
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+
+    '@media (max-width: 520px)': {
+      flexDirection: 'column',
+    },
+  },
+
+  control: {
+    height: 42,
+    fontSize: theme.fontSizes.md,
+
+    '&:not(:first-of-type)': {
+      marginLeft: theme.spacing.md,
+    },
+
+    '@media (max-width: 520px)': {
+      '&:not(:first-of-type)': {
+        marginTop: theme.spacing.md,
+        marginLeft: 0,
+      },
+    },
+  },
+
+  secondaryControl: {
+    color: theme.white,
+    backgroundColor: 'rgba(255, 255, 255, .4)',
+
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, .45) !important',
+    },
+  },
+}));
+
+const user = {  
+  name: 'Martinus Andika Novanawa',
+  email: 'martinus.novanawa@binus.ac.id',
+  image: 'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80',
+}
+const tabs = ['Home', 'Meal Plan', 'Grocery List', 'Price List', 'Article', 'News'];
+
+export default function HeroImageBackground() {
+  const { classes, cx } = useStyles();
+
+  return (
+    <div>
+      <HeaderTabs user={user} tabs={tabs}/>
+      <div className={classes.wrapper}>
+      <Overlay color="#000" opacity={0.65} zIndex={1} />
+
+      <div className={classes.inner}>
+        <Title className={classes.title}>
+          Automated AI code reviews for{' '}
+          <Text component="span" inherit className={classes.highlight}>
+            any stack
+          </Text>
+        </Title>
+
+        <Container size={640}>
+          <Text size="lg" className={classes.description}>
+            Build more reliable software with AI companion. AI is also trained to detect lazy
+            developers who do nothing and just complain on Twitter.
+          </Text>
+        </Container>
+
+          <div className={classes.controls}>
+            <Link href="/register" passHref>
+              <Button className={classes.control} variant="white" size="lg">
+                Get started
+              </Button>
+            </Link>
+            
+            <Link href="/video" passHref>
+              <Button className={cx(classes.control, classes.secondaryControl)} size="lg">
+                Live demo
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
